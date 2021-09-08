22efaacaf4db24847685cdaedd477ccdcbc2e795
Johnny Nguyen <johnnynguyenwork@gmail.com>
2021-09-08 19:47:41 +1000

    feat: Added Home Screen

--- lib/common/utils/themes.dart
+++ lib/common/utils/themes.dart
@@ -1,4 +1,5 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 /// Application's light theme
@@ -7,8 +8,8 @@ final appThemeLight = FlexColorScheme.light(
         fontFamily: GoogleFonts.poppins().fontFamily)
     .toTheme
     .copyWith(
-      textTheme: GoogleFonts.poppinsTextTheme(),
-    );
+        textTheme: GoogleFonts.poppinsTextTheme(),
+        backgroundColor: const Color(0xFFF1F3F5));
 
 /// Application's dark theme
 final appThemeDark = FlexColorScheme.dark(
@@ -17,4 +18,5 @@ final appThemeDark = FlexColorScheme.dark(
     .toTheme
     .copyWith(
       textTheme: GoogleFonts.poppinsTextTheme(),
+      backgroundColor: const Color(0xFFF1F3F5),
     );
