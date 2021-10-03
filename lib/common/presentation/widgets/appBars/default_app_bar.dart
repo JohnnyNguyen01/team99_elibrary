5216c5f4eed9d8f80b3edb9862515cc50cb87fa0
JohnnyNguyen01 <johnnynguyenwork@gmail.com>
2021-10-03 20:54:07 +1100

    Fix: home-screen overflow and added defaultInnerAppBar

---
+++ lib/common/presentation/widgets/appBars/default_app_bar.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_extension/riverpod_extension.dart';
+import 'package:riverpod_navigation/riverpod_navigation.dart';
+
+import '../../../infrastructure/routes/routes.dart';
+import '../../../utils/constants.dart';
+import '../../../utils/extensions/build_context.dart';
+import '../../../utils/hooks.dart';
+import '../mouse_region_pointer.dart';
+
+/// Default app bar to show beneath sliver
+// ignore: avoid_implementing_value_types
+class DefaultAppBar extends HookWidget implements PreferredSize {
+  /// [DefaultAppBar] constructor
+  const DefaultAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = useTheme();
+    final screenSize = useScreenSize();
+
+    final _mockRoutes = [
+      'Books',
+      'Services',
+      'Referencing',
+      'Get Help',
+      'About Us',
+      "What's On",
+      'Visit Us'
+    ];
+
+    final navigator = useProvider(navigationProvider.notifier);
+    return AppBar(
+      backgroundColor: theme.colorScheme.background,
+      centerTitle: false,
+      title: MouseRegionPointer(
+        onPressed: () => navigator.navigate(RoutePath.home),
+        child: Image.asset(
+          '$assetImagePath/wsu_logo.png',
+          height: 64,
+        ),
+      ),
+      bottom: PreferredSize(
+        preferredSize: const Size.fromHeight(navigationBarHeight),
+        child: Container(
+          width: screenSize.width,
+          height: navigationBarHeight,
+          color: theme.colorScheme.onSurface,
+          // TODO: Turn into HyperLinks or TabBar [Johnny]
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (final mockRoute in _mockRoutes)
+                // TODO: Refactor into common widget [Johnny]
+                MouseRegionPointer(
+                  onPressed: () =>
+                      context.navigateTo(routePath: RoutePath.books),
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 24),
+                    child: Text(
+                      mockRoute,
+                      style: theme.textTheme.bodyText1
+                          ?.copyWith(color: theme.colorScheme.surface),
+                    ),
+                  ),
+                )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget get child => Container();
+
+  @override
+  Size get preferredSize =>
+      const Size.fromHeight(kToolbarHeight + navigationBarHeight);
+}
--- lib/common/utils/constants.dart
+++ lib/common/utils/constants.dart
@@ -15,3 +15,19 @@ const mediumScreenWidth = 1265.0;
 
 /// small screen breakpoint
 const smallScreenWidth = 800.0;
+
+/// App Bar Height
+const appBarheight = 90.0;
+
+/// Navigation Bar Height
+const navigationBarHeight = 56.0;
+
+/// library Status Bar Height
+const libraryStatusBarHeight = 40.0;
+
+/// Search Field Container Height
+const searchContainerHeight = 220.0;
+
+/// Website Header height
+const headerHeight =
+    navigationBarHeight + libraryStatusBarHeight + searchContainerHeight;
